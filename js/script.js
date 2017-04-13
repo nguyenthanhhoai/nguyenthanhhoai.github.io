08688e4aeb7e81b4d110210c5b5c7947b3cae4d2
nguyenthanhhoai <nguyenthanhhoai0107@gmail.com>
2017-04-13 22:50:56 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   // $('.navigation').affix({offset: {top: 150} });
 
-  $('body').scrollspy({target: ".navbar", offset: 50});
+  $('body').scrollspy({target: ".navbar", offset: 80});
   
   // Add smooth scrolling on all links inside the navbar
   $("#myNavbar a, a.scroll-down-btn").on('click', function(event) {
@@ -14,7 +14,7 @@ $(document).ready(function() {
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-        scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top - 80
       }, 800, function(){
       // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
